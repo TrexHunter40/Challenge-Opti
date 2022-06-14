930c304a30b1c24a1bff89d3bcfabeee73b2399f
Maxime Lance <maxime.lance@tutanota.com>
2022-06-14 12:14:46 +0200

    calculato do costo terminato!

--- src/calculus.py
+++ src/calculus.py
@@ -9,7 +9,6 @@ car ---> idcar (int corresponding to its number in the instance file)
 class car:
     def __init__(self, idcar, nboptions, options):
         self.idcar = idcar
-        self.nbobtions = nboptions
         self.options = options
 
 class option:
@@ -28,21 +27,29 @@ class instance:
 
     def calcost(instance):
         cost = 0
-        for v in range(instance.cars):
+        for v in range(instance.nbcars):
             for o in range(instance.nboptions):
                 excess = 0
                 po = instance.options[o].weight
                 for k in range(v, v+instance.options[o].P):
-                    excess += instance.cars[k].options[o]
+                    if k < instance.nbcars:
+                        excess += instance.cars[k].options[o]
                 excess -= instance.options[o].N
                 cost += po * max(0, excess)
 
         return cost
 
 
+'''
+c0 = car(0, [1, 1])
+c1 = car(1, [0, 1])
 
 
+o0 = option(1, 2, 3, "option1")
+o1 = option(1, 4, 5, "option2")
 
+inst = instance(2, 2, [o0, o1], [c0, c1])
 
-
-        return cost
+cost = inst.calcost()
+print(cost)
+'''
