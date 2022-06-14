500c609b6bd153594593abc6d411ecd5a2727a57
Maxime Lance <maxime.lance@tutanota.com>
2022-06-14 17:21:52 +0200

    FIX: pupupupute outpute.py

--- src/outpute.py
+++ src/outpute.py
@@ -1,9 +1,11 @@
 import calculus as cal
+import os
 
 # n -> numero de l'instance
 
 def export(inst, n):
-    f = open("res_"+str(n)+".txt", "w")
+    cur_path = os.path.dirname(__file__)
+    f = open(cur_path + "/Instances/res_"+str(n)+".txt", "w")
     f.write("EQUIPE monke\n")
     f.write("INSTANCE "+str(n)+"\n")
     for c in inst.cars:
