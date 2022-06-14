c2d1977326e8fb6ec3294a23afc990728e75ecbe
Maxime Lance <maxime.lance@tutanota.com>
2022-06-14 17:38:46 +0200

    Revert "FIX: pupupupute outpute.py"
    This reverts commit 500c609b6bd153594593abc6d411ecd5a2727a57.

--- src/outpute.py
+++ src/outpute.py
@@ -1,11 +1,9 @@
 import calculus as cal
-import os
 
 # n -> numero de l'instance
 
 def export(inst, n):
-    cur_path = os.path.dirname(__file__)
-    f = open(cur_path + "/Instances/res_"+str(n)+".txt", "w")
+    f = open("res_"+str(n)+".txt", "w")
     f.write("EQUIPE monke\n")
     f.write("INSTANCE "+str(n)+"\n")
     for c in inst.cars:
