07e8d3b6fdcd6ab9858792fae1a2c538e6094046
Léo Chevalier <leocheche40@gmail.com>
2022-06-14 11:48:32 +0200

    FEAT: def string_ectract

--- src/import.py
+++ src/import.py
@@ -1,13 +1,32 @@
 import os
 
+def string_extract(str) :
+    res = ''
+
+    for i in str :
+        if i.isdigit():
+            res += i
+
+    res = int(res)
+
+    return res
+
+
+
 def import_file():
 
     cur_path = os.path.dirname(__file__)
     file_path = cur_path + "/Instances/cs1.txt"
 
     file = open(file_path, "rt")
-    print(file.read())
+    #print(file.read())
 
-    
+    line1 = file.readline()
+    line2 = file.readline()
 
-import_file()
+    print(line1+line2)
+
+print("\n")
+
+print(string_extract("ABc 36"))
+#import_file()
